b48550d70d9613e3aa7bc010e9f5dc8490ebfc9c
Boris Chepurnoy <beastbs.dev@gmail.com>
2022-12-21 13:01:22 +0200

    added fetch to fb,added render questions in ui

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const webpackConfig = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.[chunkhash].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+	rules: [
+	  {
+		 test: /\.css$/i,
+		 use: ["style-loader", "css-loader"],
+	  },
+	],
+ },
+};
+
+module.exports = webpackConfig;
